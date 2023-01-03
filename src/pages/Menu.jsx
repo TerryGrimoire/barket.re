4f1200554caee42db7ec9b6958ef1d831726a473
Terry Grimoire <terry.grimoire@gmail.com>
2023-01-04 01:45:00 +0400

    adding css

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,31 +4,83 @@ import { Helmet } from "react-helmet-async";
 
 function Menu({ helmet }) {
   const [menu, setMenu] = useState([]);
+  const [recherche, setRecherche] = useState([]);
 
   useEffect(() => {
     window.scrollTo(0, 0);
     setMenu(JSON.parse(sessionStorage.getItem("menus")));
+    setRecherche(JSON.parse(sessionStorage.getItem("recherche")));
   }, []);
 
   return (
-    <div className="Menu_page">
+    <div className="menu_page">
       <Helmet>
         <title>{helmet.title} |</title>
         <link rel="canonical" href={`${helmet.href}/Menu`} />
         <meta name="description" content={helmet.description} />
       </Helmet>
-      <section>
-        {menu.map((el) => (
-          <div key={el.Plat}>
-            <h3>{el.Plat}</h3>
-            <p>{el.Prix}</p>
-            <p>{el.Description}</p>
-            <Link to={`/Restaurants/${el.Nom}`}>
-              {el.Nom.replace("_", " ")}
-            </Link>
+      <main>
+        <section className="menu_top">
+          <h1>Les Menus du jour</h1>
+          <div>
+            <p>trier</p>
           </div>
-        ))}
-      </section>
+        </section>
+        <section
+          className={
+            recherche.choix === "Menu du jour"
+              ? "menu_middle"
+              : "menu_middle reverse"
+          }
+        >
+          <section>
+            <h2>Les plats salés du jour</h2>
+            {menu
+              .filter(
+                (el) =>
+                  el.Ville.toLowerCase().includes(
+                    recherche.city.toLowerCase()
+                  ) && el.Type.toLowerCase().includes("repas")
+              )
+              .map((el) => (
+                <div key={el.Plat}>
+                  <div className="top">
+                    <h3>
+                      {el.Plat} de{"  "}
+                      <Link to={`/Restaurants/${el.Nom}`}>
+                        <span>{el.Nom.replace("_", " ")}</span>
+                      </Link>
+                    </h3>
+                    <h3>{el.Prix}</h3>
+                  </div>
+                </div>
+              ))}
+          </section>
+          <section>
+            <h2>Les desserts du jour</h2>
+            {menu
+              .filter(
+                (el) =>
+                  el.Ville.toLowerCase().includes(
+                    recherche.city.toLowerCase()
+                  ) && el.Type.toLowerCase().includes("dessert")
+              )
+              .map((el) => (
+                <div key={el.Plat}>
+                  <div className="top">
+                    <h3>
+                      {el.Plat} de{"  "}
+                      <Link to={`/Restaurants/${el.Nom}`}>
+                        <span>{el.Nom.replace("_", " ")}</span>
+                      </Link>
+                    </h3>
+                    <h3>{el.Prix}</h3>
+                  </div>
+                </div>
+              ))}
+          </section>
+        </section>
+      </main>
     </div>
   );
 }
