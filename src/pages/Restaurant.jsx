fa5742dfaf356921e7a3ee36b44e3d553c64afd0
Terry Grimoire <terry.grimoire@gmail.com>
2023-01-09 08:35:02 +0400

    improving the Restaurant page

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -68,124 +68,6 @@ function Restaurant({ helmet }) {
         </>
       )}
 
-      <section className="horaires_joursferies light_bg">
-        <div className="contact_horaires light_bg">
-          <h2>Nos horaires</h2>
-          {/* Si l'entreprise ne ferme pas le midi */}
-
-          {
-            /* Si l'entreprise ferme le midi  */
-            resto[0].Lundi20 !== "" && (
-              <div>
-                <div>
-                  Lundi :{" "}
-                  {resto[0].LundiO === "Fermé" && resto[0].Lundi2O === "Fermé"
-                    ? resto[0].LundiO
-                    : `${resto[0].LundiO} - ${resto[0].LundiF} | ${
-                        resto[0].Lundi2O === "Fermé" &&
-                        resto[0].Lundi2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Lundi2O} - ${resto[0].Lundi2F}`
-                      }`}
-                </div>
-                <div>
-                  Mardi :{" "}
-                  {resto[0].MardiO === "Fermé" && resto[0].Mardi2O === "Fermé"
-                    ? resto[0].MardiO
-                    : `${resto[0].MardiO} - ${resto[0].MardiF} | ${
-                        resto[0].Mardi2O === "Fermé" &&
-                        resto[0].Mardi2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Mardi2O} - ${resto[0].Mardi2F}`
-                      }`}
-                </div>
-                <div>
-                  Mercredi :{" "}
-                  {resto[0].MercrediO === "Fermé" &&
-                  resto[0].Mercredi2O === "Fermé"
-                    ? resto[0].MercrediO
-                    : `${resto[0].MercrediO} - ${resto[0].MercrediF} | ${
-                        resto[0].Mercredi2O === "Fermé" &&
-                        resto[0].Mercredi2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Mercredi2O} - ${resto[0].Mercredi2F}`
-                      }`}
-                </div>
-                <div>
-                  Jeudi :{" "}
-                  {resto[0].JeudiO === "Fermé" && resto[0].Jeudi2O === "Fermé"
-                    ? resto[0].JeudiO
-                    : `${resto[0].JeudiO} - ${resto[0].JeudiF} | ${
-                        resto[0].Jeudi2O === "Fermé" &&
-                        resto[0].Jeudi2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Jeudi2O} - ${resto[0].Jeudi2F}`
-                      }`}
-                </div>
-                <div>
-                  Vendredi :{" "}
-                  {resto[0].VendrediO === "Fermé" &&
-                  resto[0].Vendredi2O === "Fermé"
-                    ? resto[0].VendrediO
-                    : `${resto[0].VendrediO} - ${resto[0].VendrediF} | ${
-                        resto[0].Vendredi2O === "Fermé" &&
-                        resto[0].Vendredi2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Vendredi2O} - ${resto[0].Vendredi2F}`
-                      }`}
-                </div>
-                <div>
-                  Samedi :{" "}
-                  {resto[0].SamediO === "Fermé" && resto[0].Samedi2O === "Fermé"
-                    ? resto[0].SamediO
-                    : `${resto[0].SamediO} - ${resto[0].SamediF} | ${
-                        resto[0].Samedi2O === "Fermé" &&
-                        resto[0].Samedi2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Samedi2O} - ${resto[0].Samedi2F}`
-                      }`}
-                </div>
-                <div>
-                  Dimanche :{" "}
-                  {resto[0].DimancheO === "Fermé" &&
-                  resto[0].Dimanche2O === "Fermé"
-                    ? resto[0].DimancheO
-                    : `${resto[0].DimancheO} - ${resto[0].DimancheF} | ${
-                        resto[0].Dimanche2O === "Fermé" &&
-                        resto[0].Dimanche2F === "Fermé"
-                          ? "Fermé"
-                          : `${resto[0].Dimanche2O} - ${resto[0].Dimanche2F}`
-                      }`}
-                </div>
-              </div>
-            )
-          }
-        </div>
-
-        <div className="contact_feries light_bg">
-          {resto[0].ouvert ? (
-            <div>
-              <h2> Jours fériés</h2>
-
-              <p>
-                Nous sommes fermés les jours fériés à l'exception des jours
-                suivants :{" "}
-              </p>
-
-              <ul>
-                {Object.keys(resto[0].ouvert).map((el) => (
-                  <p className="contact_li" key={el}>
-                    ▫️ {el}
-                  </p>
-                ))}
-              </ul>
-            </div>
-          ) : (
-            <p>Nous sommes fermés les jours fériés</p>
-          )}
-        </div>
-      </section>
-
       <section className="paiements">
         <h2>Moyens de paiement acceptés</h2>
         {resto[0].paiement.length <= 1 ? (
@@ -199,7 +81,7 @@ function Restaurant({ helmet }) {
         <h2>La carte</h2>
         <div>
           <h3>Les plats</h3>
-          {menu && (
+          {menu.filter((okok) => okok.Type !== "Dessert").length > 0 ? (
             <>
               {menu
                 .filter((el) => el.Type !== "Dessert")
@@ -221,11 +103,13 @@ function Restaurant({ helmet }) {
                   </Link>
                 ))}
             </>
+          ) : (
+            <p>Ce restaurant n'a pas mis à jour son menu.</p>
           )}
         </div>
         <div>
           <h3>Les desserts</h3>
-          {menu && (
+          {menu.filter((ok) => ok.Type === "Dessert").length > 0 ? (
             <>
               {menu
                 .filter((el) => el.Type === "Dessert")
@@ -247,6 +131,8 @@ function Restaurant({ helmet }) {
                   </Link>
                 ))}
             </>
+          ) : (
+            <p>Ce restaurant ne propose pas de dessert pour le moment.</p>
           )}
         </div>
       </section>
