b43cd0d31144eba39bb0264df5f3a39006176679
Terry Grimoire <terry.grimoire@gmail.com>
2023-01-10 12:58:55 +0400

    okok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -464,43 +464,6 @@ export default function Home({ helmet }) {
           className="plat"
         />
       </div>
-      <section className="white">
-        <h2>
-          Promouvoir les petits restaurateurs c'est soutenir l'économie locale.
-        </h2>
-
-        <article>
-          <h3>Barket.re, Lorem ipsum dolor sit amet. </h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure,
-            asperiores? Iusto unde tenetur perspiciatis sed quidem corrupti, hic
-            cupiditate ex possimus quam itaque eos error laborum praesentium
-            dolore repellat laboriosam iure provident est minus quod, recusandae
-            sit perferendis. Ea nobis dolor perspiciatis, blanditiis quidem
-            nulla laudantium est numquam delectus similique repellendus!
-            Reiciendis, sunt.
-          </p>
-
-          <p>
-            Itaque praesentium id, amet tempora debitis totam ipsa non corporis
-            beatae rerum ullam ipsum modi! Similique ipsum molestias
-            exercitationem blanditiis placeat temporibus non nemo quia ipsa
-            saepe perferendis numquam, veritatis fuga, aspernatur tempore
-            voluptas vero officia inventore cupiditate mollitia repellendus
-            porro explicabo molestiae! Architecto nesciunt debitis tenetur,
-            magni non fuga dolore provident deleniti ducimus excepturi at iusto,
-            adipisci saepe expedita reprehenderit soluta cum laudantium
-            voluptate ab.
-          </p>
-
-          <p>
-            Debitis, id? Repudiandae, beatae vero! Quia dolorum odio
-            exercitationem ab quis unde aut, facere distinctio possimus nisi
-            laborum magnam sunt, debitis dicta nam consectetur eum nemo rerum
-            nostrum eligendi impedit illum.
-          </p>
-        </article>
-      </section>
     </main>
   );
 }
